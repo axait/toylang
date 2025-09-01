6bcfc59aacd6aa3f8c2c662da759eac02a6f67dd
Axait-th <axaitth@gmail.com>
2025-09-01 18:00:24 +0500

    complete playground and its btns ui only.

--- src/Components/PlaygroundTitle.tsx
+++ src/Components/PlaygroundTitle.tsx
@@ -2,14 +2,31 @@ import React from 'react'
 
 const PlaygroundTitle = () => {
   return (
-    <div>
+    <div
+    className='
+      flex flex-row
+      justify-between
+      pl-2 pr-5
+    '
+    >
       <h2
       className='
       md:mt-5
       text-[0.9rem] font-mono
       font-bold
+      pt-2
       '
       >Playground:</h2>
+      <div
+      className='
+      md:mt-5
+      text-[0.9rem] font-mono
+      font-bold
+      '
+      >
+        <button className="btn btn-error font-mono h-[35px]">Run</button>
+        <button className="btn btn-soft btn-error random-code-btn ml-2 h-[35px]">Random</button>
+      </div>
       
     </div>
   )
