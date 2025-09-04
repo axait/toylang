27208e16f5180279ba4609607cceae3b9058bfce
Axait-th <axaitth@gmail.com>
2025-09-04 17:11:04 +0500

    fix github icon issue

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,4 +1,5 @@
 import { Copyright } from 'lucide-react';
+import GithubLogo from '../assets/github-logo.png'
 
 const Footer = () => {
     return (
@@ -9,7 +10,7 @@ const Footer = () => {
             <div className='flex flex-row h-[10]' ><Copyright height={17} className='mt-0.5' />{new Date().getFullYear()}</div>
 
             <a href="http://github.com/axait/toylang" target="_blank" rel="noreferrer" className='flex flex-row ml-3'>
-                <img src="https://github.githubassets.com/favicons/favicon.svg" width="20" height="20" alt="githublogo" />
+                <img src={GithubLogo} className='w-[20px] h-[20px]' alt="githublogo" />
                 &nbsp;axait
             </a>
         </div>
