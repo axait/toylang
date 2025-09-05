3600daf6740b6cbf158c4f2a25b8ea2ec7a87cb7
Axait-th <axaitth@gmail.com>
2025-09-05 17:12:54 +0500

    shift microst clarity to index.html

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,9 @@ import MyConsole from './Components/MyConsole.tsx'
 import Docs from './Components/Docs.tsx'
 import Footer from './Components/Footer.tsx'
 import CustomCursor from './Components/CustomCursor.tsx'
-import { useEffect } from 'react';
 // import TempFile from './Components/TempFile.tsx'
-import Clarity from "@microsoft/clarity";
 
 function App() {
-	useEffect(() => {
-		// if (process.env.NODE_ENV === "production") {
-			Clarity.init("t5zc2cwizx"); // 👈 your Clarity project ID
-		// }
-	}, []);
-
 	return (
 		<>
 			<Banner />
