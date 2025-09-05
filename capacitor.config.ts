24aa54700e4e38686bb40e016330da4457560839
Axait-th <axaitth@gmail.com>
2025-09-05 18:28:59 +0500

    finally ionic has been setup.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 const config = {
   appId: 'com.toylang.app',
   appName: 'ToylangApp',
-  webDir: 'build',  // <-- this must point to your React build folder
+  webDir: 'dist',  // <-- this must point to your React build folder
   bundledWebRuntime: false
 };
 export default config;
