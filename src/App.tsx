47ac1416379285819daadb3a89d65f344a614a71
Axait-th <axaitth@gmail.com>
2025-09-05 16:38:48 +0500

    add microsoft clarity.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
 import './styles/App.scss'
-import "prismjs/themes/prism-tomorrow.css"; 
+import "prismjs/themes/prism-tomorrow.css";
 import Banner from './Components/Banner.tsx'
 import CoderEditor from './Components/CodeEditor.tsx'
 import MyConsole from './Components/MyConsole.tsx'
 import Docs from './Components/Docs.tsx'
 import Footer from './Components/Footer.tsx'
 import CustomCursor from './Components/CustomCursor.tsx'
+import { useEffect } from 'react';
 // import TempFile from './Components/TempFile.tsx'
+import Clarity from "@microsoft/clarity";
 
 function App() {
+	useEffect(() => {
+		// if (process.env.NODE_ENV === "production") {
+			Clarity.init("t5zc2cwizx"); // 👈 your Clarity project ID
+		// }
+	}, []);
 
 	return (
 		<>
-		<Banner/>
-		<CoderEditor/>
-		<MyConsole />
-		<Docs/>
-		<Footer/>
-		<CustomCursor/>
-		{/* <TempFile/> */}
+			<Banner />
+			<CoderEditor />
+			<MyConsole />
+			<Docs />
+			<Footer />
+			<CustomCursor />
+			{/* <TempFile/> */}
 		</>
 	)
 }
