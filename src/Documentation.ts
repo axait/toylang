d087f4bfd5e44600e32f58a267d39be21f444122
Axait-th <140580384+axait@users.noreply.github.com>
2025-09-16 15:48:45 +0500

    Update Documentation.ts

--- src/Documentation.ts
+++ src/Documentation.ts
@@ -12,21 +12,21 @@ export const DocumentationArray: DocArticleProps[] = [
         title: "Variable",
         description: "A variable can be declare by using `declare` keyword.",
         codeExample: "declare name\ninput(\"Enter your name: \", name)\np(\"Welcome, \")\np(name)\n",
-        isNew: true,
+        isNew: false,
         tags: ["keyword", "input", "print"]
     },
     {
         title: "Input",
         description: "The input function is used to get user input.",
         codeExample: "declare name\ninput(\"Enter your name: \", name)\np(name)",
-        isNew: true,
+        isNew: false,
         tags: ["input"]
     },
     {
         title: "Print",
         description: "The p function is used to print output.",
         codeExample: "p(\"Hello, World!\")",
-        isNew: true,
+        isNew: false,
         tags: ["print"]
     },
 ]
